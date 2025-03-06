45b58e4eb59a44a3934edcdaf66da331addf834b
Wellington Santos <dev.wsantos@outlook.com>
2025-03-06 14:36:47 -0300

    formatting date with SimpleDateFormat object and getting to know the Calendar object.

--- src/dates_module/DatesInJava.java
+++ src/dates_module/DatesInJava.java
@@ -1,17 +1,18 @@
 package dates_module;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class DatesInJava {
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		
 		Date date = new Date();
 		
 		// milliseconds that represent a current date
 		System.out.println("Date in milliseconds: " + date.getTime());
-		
-		
+				
 		/* deprecated methods */
 		
 		// day of Month
@@ -34,6 +35,16 @@ public class DatesInJava {
 		
 		
 		
+		/*----------------- Simple Date Format -------------------------*/
+		System.out.println("-------------------------------------------------");
+		
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+		System.out.println("Date to show on screen: " + simpleDateFormat.format(date));
+		
+		simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("Date to database format: " + simpleDateFormat.format(date));
+		
+		System.out.println(simpleDateFormat.parse("1985-10-20 23:25:00"));
 		
 	}
 }
